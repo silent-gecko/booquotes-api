7342cf636c47df12b6aa5bccba5978a7493be5aa
alenameksheneva <alenameksheneva@gmail.com>
2021-08-27 21:22:11 +0300

    Added migration + model + seeder for USER entity

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->count(5)->create();
+    }
+}
