d39ae39e6a98d4d780f8e8e3f93f68167c71a37a
alenameksheneva <alenameksheneva@gmail.com>
2021-08-27 21:32:42 +0300

    Added migration for Author entity

---
+++ database/migrations/2021_08_27_212517_create_authors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->uuid('id')->primary()->default(DB::raw('uuid_generate_v4()'));
+            $table->string('name');
+            $table->text('bio')->nullable();
+            $table->year('year_of_birth');
+            $table->year('year_of_death')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
