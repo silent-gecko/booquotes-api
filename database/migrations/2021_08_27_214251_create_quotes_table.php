7d3a9f37e037281175a4be2fe0d9b1f0ceffff0d
alenameksheneva <alenameksheneva@gmail.com>
2021-08-27 22:02:52 +0300

    Added migration for Quote entity

---
+++ database/migrations/2021_08_27_214251_create_quotes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quotes', function (Blueprint $table) {
+            $table->uuid('id')->primary()->default(DB::raw('uuid_generate_v4()'));
+            $table->text('text');
+            $table->uuid('book_id');
+            $table->timestamps();
+
+            $table->foreign('book_id')
+                ->references('id')
+                ->on('books')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quotes');
+    }
+}
