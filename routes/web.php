61b14e858416ab9b9ec2cbcc10aaa1c51f422bc0
alenameksheneva <alenameksheneva@gmail.com>
2021-08-30 16:47:42 +0300

    Added route name

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 */
 
 $router->group(['prefix' => 'api/v1', 'middleware' => 'auth'], function () use ($router) {
-    $router->get('/', function () use ($router) {
+    $router->get('/', ['as' => 'home', function () use ($router) {
         return $router->app->version();
-    });
+    }]);
 });
