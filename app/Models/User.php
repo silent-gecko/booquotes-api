750157f4e7ae5fc9d4484eff3a0088e6d90478b1
alenameksheneva <alenameksheneva@gmail.com>
2021-08-30 16:46:04 +0300

    Edited User model dependencies

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Auth\Authenticatable;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Lumen\Auth\Authorizable;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class User extends Model
 {
-    use Authenticatable, Authorizable, HasFactory;
+    use HasFactory;
 
     /**
      * Indicates if the model's ID is auto-incrementing.
