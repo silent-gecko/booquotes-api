5297076dcb2ffe7298ae2d4dc8b78f543009613e
alenameksheneva <alenameksheneva@gmail.com>
2021-08-31 18:40:29 +0300

    Applied response macro

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -36,7 +36,7 @@ class Authenticate
     public function handle($request, Closure $next, $guard = null)
     {
         if ($this->auth->guard($guard)->guest()) {
-            return response()->json(['code' => 401, 'success' => false, 'message' => 'Unauthorized'], 401);
+            return response()->jsonError(401, 'Unauthorized');
         }
 
         return $next($request);
