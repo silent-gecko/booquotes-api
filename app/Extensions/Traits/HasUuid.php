b6d31de3ee236bf32f2eca3fc34a2608dcc6240e
alenameksheneva <alenameksheneva@gmail.com>
2021-09-02 17:06:45 +0300

    Added uuid model handler trait

---
+++ app/Extensions/Traits/HasUuid.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Extensions\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Ramsey\Uuid\Uuid;
+
+trait HasUuid
+{
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
+
+    protected static function bootHasUuid() {
+        static::creating(function (Model $model) {
+            if (!$model->getKey() || !Str::isUuid($model->getKey())) {
+                $model->setAttribute($model->getKeyName(), Str::uuid());
+            }
+        });
+    }
+}
