fa0eefa282e9783ba7ef6861f070452a1750e1b1
alenameksheneva <alenameksheneva@gmail.com>
2021-09-05 17:49:09 +0300

    Added new Books table field

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Extensions\Traits\HasUuid;
 
 class Book extends Model
 {
+    use HasUuid;
+
+    const SORT_INDEX_LENGTH = 3;
+
     /**
      * Indicates if the model's ID is auto-incrementing.
      *
@@ -36,6 +41,13 @@ class Book extends Model
         'title', 'description', 'author_id'
     ];
 
+    /**
+     * The number of models to return for pagination.
+     *
+     * @var int
+     */
+    protected $perPage = 10;
+
 
     /**
      * Get the author of the book.
@@ -51,5 +63,20 @@ class Book extends Model
     public function quotes()
     {
         return $this->hasMany(Quote::class);
+    }
+
+    /**
+     * Set sorting index from title value
+     * @param $value
+     */
+    public function setTitleAttribute($value): void
+    {
+        $modifiedTitle = trim($value);
+        $this->attributes['title'] = $modifiedTitle;
+
+        $modifiedTitle = explode(' ', mb_strtolower($modifiedTitle));
+        $sortingLetters = substr($modifiedTitle[0] == 'the' ? $modifiedTitle[1] : $modifiedTitle[0], 0,
+            self::SORT_INDEX_LENGTH);
+        $this->attributes['sort_index'] = $sortingLetters;
     }
 }
