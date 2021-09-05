32d86e3fcccc5028a95b8ee7bffe3bce204b0712
alenameksheneva <alenameksheneva@gmail.com>
2021-09-05 18:13:39 +0300

    Added resource

---
+++ app/Http/Resources/BookResource.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     *
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'title'  => $this->title,
+            'links' => [
+                'self'   => $this->self_link,
+                'author'  => $this->aithor_link,
+                'quotes' => $this->quotes_link,
+            ]
+        ];
+    }
+
+    /**
+     * Get additional data that should be returned with the resource array.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function with($request)
+    {
+        return [
+            'data' => [
+                'description'  => $this->description,
+            ]
+        ];
+    }
+}
