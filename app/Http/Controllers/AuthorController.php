61debec41dfa1ec12b49fe955fc4ae664135e611
alenameksheneva <alenameksheneva@gmail.com>
2021-09-29 17:17:52 +0300

    WIP: added request transformation layer

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreAuthorRequest;
 use App\Models\Author;
 use App\Http\Resources\AuthorResource;
 use App\Http\Resources\AuthorCollection;
+use App\Rules\Year;
 use Illuminate\Http\Request;
 
 class AuthorController extends Controller
@@ -30,5 +32,13 @@ class AuthorController extends Controller
     public function show(string $uuid)
     {
         return new AuthorResource(Author::findOrFail($uuid));
+    }
+
+    public function store(StoreAuthorRequest $request)
+    {
+        $validated = $request->validated();
+        $author = Author::create($validated);
+
+        return response()->jsonCreated($author->id);
     }
 }
